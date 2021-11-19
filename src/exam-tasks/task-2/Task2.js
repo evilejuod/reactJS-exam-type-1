7d45d8852c446a5be97b0a2103e05296342cb82b
Evile <evile.juodziukynaite@gmail.vom>
2021-11-19 17:26:54 +0200

    third commit

--- src/exam-tasks/task-2/Task2.js
+++ src/exam-tasks/task-2/Task2.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  font-size: 30px;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+`;
+const Subtitle = styled.h3`
+  text-transform: uppercase;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 35vh;
+`;
+const CardsDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 30px;
+`;
+
+const Img = styled.img`
+  height: 200px;
+  margin: 0 10px;
+`;
+const Card = styled.div`
+display: flex;
+  flex-direction: row;
+  margin: 10px 0;
+`;
+
+const categoriesData = [
+    {
+        id: 1,
+        title: 'posters',
+        src: "./posters.jpg",
+    },
+    {
+        id: 2,
+        title: 'apparel',
+        src: "./apparel.jpg",
+    },
+    {
+        id: 3,
+        title: 'stickers',
+        src: "./stickers.jpg",
+    },
+    {
+        id: 4,
+        title: 'cups',
+        src: "./cups.jpg",
+    },
+    {
+        id: 5,
+        title: 'calendars',
+        src: './calendars.jpg',
+    },
+]
 
 function Task2() {
   return (
     <div>
       <h3>Task 2</h3>
+        <CardsDiv >
+            <Title>Categories</Title>
+            <Card>
+                {categoriesData.map((item) => {
+                    return (
+                        <Div key={item.id} >
+                            <Img src={item.src} alt=''/>
+                            <Subtitle>{item.title}</Subtitle>
+                        </Div>
+
+                    )})}
+            </Card>
+
+        </CardsDiv>
     </div>
   );
 }
--- src/exam-tasks/task-3/Task3.js
+++ src/exam-tasks/task-3/Task3.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
+
+const Form = styled.form`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 70px;
+`;
+const Input = styled.input`
+width: 300px;
+  height: 30px;
+  padding: 0 10px;
+  margin-bottom: 10px;
+`;
+const Button = styled.button`
+  font-size: 15px;
+  background-color: #051e28;
+  color: whitesmoke;
+  border: none;
+  height: 35px;
+  width: 325px;
+  border-radius: 3px;
+   :hover{
+    background-color: #6dc26a;
+  }
+`;
+const Text = styled.p`
+margin-top: 10px;
+  
+`;
 
 function Task3() {
+
+    const [ inputs, setInputs ] = useState({
+        name: '',
+        surname: '',
+        age: '',
+    })
+    const [msg, setMsg] = useState('')
+
+    const handleInputs = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { name, surname, age } = inputs;
+        if(age >= 18 ){
+            setMsg('Dėkojame, kad užsiregistravote')
+        }
+        if(age <= 17 ){
+            setMsg('Dėja, registracija negalima. Registruotis galima tik nuo 18 metų')
+        }
+    }
+
+
   return (
-    <div>
+    <div style={{ height: '60vh' }}>
       <h3>Task 3</h3>
+            <Form onSubmit={handleSubmit}>
+                <Input
+                    type='text'
+                    placeholder='Vardas'
+                    name='name'
+                    onChange={handleInputs}
+                    value={inputs.name}
+
+                />
+                <Input
+                    type='text'
+                    placeholder='Pavardė'
+                    name='surname'
+                    onChange={handleInputs}
+                    value={inputs.surname}
+                />
+                <Input
+                    type='number'
+                    placeholder='Amžius'
+                    name='age'
+                    onChange={handleInputs}
+                    value={inputs.age}
+                />
+                <Button type='submit'>Pateikti</Button>
+                <Text>{msg}</Text>
+            </Form>
+
     </div>
   );
 }
